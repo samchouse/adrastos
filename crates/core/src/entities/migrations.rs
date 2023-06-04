3b0569f556a9a24b37393edc66016b0a79317a4e
Samuel Corsi-House <chouse.samuel@gmail.com>
2023-06-04 10:49:03 -0400

    fix: linting

--- crates/core/src/entities/migrations.rs
+++ crates/core/src/entities/migrations.rs
@@ -1,5 +1,6 @@
 use super::{custom_table::schema::CustomTableSchema, Connection, Migrate, RefreshTokenTree, User};
 
+// TODO(@Xenfo): don't run if not necessary
 pub async fn migrate(db_pool: &deadpool_postgres::Pool) {
     let conn = db_pool.get().await.unwrap();
     let migrations = vec![
