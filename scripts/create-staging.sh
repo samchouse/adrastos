09651b2d5144fe19b0cf85ad4ccd49cd0627eebf
Samuel Corsi-House <chouse.samuel@gmail.com>
2023-08-22 07:50:27 -0400

    ci: fix

---
+++ scripts/create-staging.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PR=$1
+PORT=$2
+
+mkdir -p data/staging/conf.d
+
+cat <<EOF > "data/staging/conf.d/adrastos-pr-$PR.xenfo.dev.conf"
+server {
+  listen 80;
+  listen [::]:80;
+  server_name adrastos-pr-$PR.xenfo.dev;
+
+  # reverse proxy
+  location / {
+    proxy_pass http://127.0.0.1:$2;
+    proxy_set_header Host \$host;
+    include nginxconfig.io/proxy.conf;
+  }
+
+  # additional config
+  include nginxconfig.io/general.conf;
+}
+EOF
