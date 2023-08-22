e5e518503237d0afb91cc4debe45169a282ee19d
Samuel Corsi-House <chouse.samuel@gmail.com>
2023-08-22 12:30:46 -0400

    ci: getting closer

--- scripts/create-staging.sh
+++ scripts/create-staging.sh
@@ -3,6 +3,7 @@
 PR=$1
 PORT=$2
 
+mkdir -p staging
 mkdir -p data/staging/conf.d
 
 cat <<EOF > "data/staging/conf.d/adrastos-pr-$PR.xenfo.dev.conf"
@@ -22,3 +23,35 @@ server {
   include nginxconfig.io/general.conf;
 }
 EOF
+
+cat <<EOF > "staging/docker-compose.pr-$PR.yml"
+version: '3.9'
+
+services:
+  emails:
+    image: ghcr.io/xenfo/adrastos-emails:pr-$PR
+    pull_policy: always
+    restart: unless-stopped
+    env_file:
+      - staging.env
+    networks:
+      - adrastos_default
+
+  app:
+    image: ghcr.io/xenfo/adrastos-app:pr-$PR
+    pull_policy: always
+    restart: unless-stopped
+    depends_on:
+      - emails
+    env_file:
+      - staging.env
+    networks:
+      - adrastos_default
+    ports:
+      - $PORT:8000
+    volumes:
+      - ~/.postgresql/root.crt:/work/certs/cockroach.crt
+EOF
+
+docker compose -f "staging/docker-compose.pr-$PR.yml" up -d
+docker exec adrastos-staging-nginx-1 /usr/sbin/nginx -s reload
