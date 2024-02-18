5fd670d7d21a442afdba15316fb387d027241f4b
Samuel Corsi-House <chouse.samuel@gmail.com>
2024-02-17 19:04:06 -0500

    chore: formatting

--- crates/macros/src/derive/db_query.rs
+++ crates/macros/src/derive/db_query.rs
@@ -124,27 +124,27 @@ pub fn db_query(item: TokenStream) -> TokenStream {
                         let Some(error) = crate::db::postgres::Error::try_from(routine).ok() else {
                             return crate::error::Error::InternalServerError("Unsupported database error code".to_string());
                         };
-            
+
                         match error {
                             crate::db::postgres::Error::UniqueKeyViolation => {
                                 let pre = regex::Regex::new(r"\(.+\)=\('.+'\)").unwrap();
-            
+
                                 let Some(detail) = db_error.detail() else {
                                     return crate::error::Error::InternalServerError("Unable to get error info".to_string());
                                 };
                                 let Some(matched) = pre.find(detail) else {
                                     return crate::error::Error::InternalServerError("Invalid error details".to_string());
                                 };
-            
+
                                 let mut details = matched.as_str().split('=').collect::<Vec<_>>().into_iter();
-            
+
                                 let Some(key) = details.next() else {
                                     return crate::error::Error::InternalServerError("Invalid error details".to_string());
                                 };
                                 let Some(value) = details.next() else {
                                     return crate::error::Error::InternalServerError("Invalid error details".to_string());
                                 };
-            
+
                                 crate::error::Error::BadRequest(format!(
                                     "Key '{}' already exists with value '{}'",
                                     key.replace(['(', ')'], ""),
